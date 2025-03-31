926abd040693bd3fd0bd93cc6f9471daca889ac2
al3168 <alessandra.guida89@gmail.com>
2025-03-31 11:20:25 +0200

    Add files via upload

---
+++ lunedì_31_03/lunedì_31_03/esercizi_condizioni.py
@@ -0,0 +1,11 @@
+
+# prendere un numero dall'utente 
+
+eta_utente = int(input("scrivimi la tua età ?"))
+#verificare la prima condizione 
+
+if eta_utente < 18:
+    print("mi dispiace, non puoi vedere questo film")
+    
+else:
+  print("puoi vedere questo film ")
